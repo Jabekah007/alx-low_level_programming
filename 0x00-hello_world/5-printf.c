758385823a5db24a9624287228e1dc554b242997
Justice Abekah <jabekah007@gmail.com>
2023-02-16 15:29:05 -0800

    art is life

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -  a program to print art fact
+ * Return: 0 (Successful!!!)
+ */
+int main(void)
+{
+printf("\"with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
