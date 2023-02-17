8a7a4b43f7752e4bc313518e2ee5d4cae0856dcb
Justice Abekah <jabekah007@gmail.com>
2023-02-16 16:32:53 -0800

    datatype size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main -  a program to print art fact
+ * Return: 0 (Successful!!!)
+ */
+int main(void)
+{
+int age;
+double double_num;
+char character;
+float float_num;
+long long_num;
+long long int long_int;
+
+printf("int size = %zu", sizeof(age));
+printf("\ndouble size = %zu", sizeof((double_num));
+printf("\nfloat size = %zu", sizeof((float_num));
+printf("\nchar size = %zu", sizeof((character));
+printf("\nlong size = %zu", sizeof((long_num)));
+printf("\nlong long int size = %zu", sizeof((long_int));
+return (0);
+}
