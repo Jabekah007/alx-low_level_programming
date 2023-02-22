19aed217d582e4385e2d144ef490cc7873165acb
Justice Abekah <jabekah007@gmail.com>
2023-02-22 09:39:55 -0800

    debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,4 +4,8 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-#endif/* MAIN_H */
+int largest_number(int a,int b,int c);
+void print_remaining_days(int month,int day,int year);
+void print_times_table(int n);
+
+#endif /* MAIN_H */
